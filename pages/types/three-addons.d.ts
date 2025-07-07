6877239cf111eb46ba77bcc4f3b0aaedfd1454a5
dexArya <arya.dexy@gmail.com>
2025-07-08 00:57:36 +0700

    fix three-addons.d.ts

--- pages/types/three-addons.d.ts
+++ pages/types/three-addons.d.ts
@@ -1,18 +1,8 @@
 declare module 'three/examples/jsm/loaders/FontLoader' {
   import { Loader } from 'three';
-  import { Font } from 'three/examples/jsm/loaders/FontLoader';
-
-  export class FontLoader extends Loader {
-    load(
-      url: string,
-      onLoad: (font: Font) => void,
-      onProgress?: (event: ProgressEvent<EventTarget>) => void,
-      onError?: (event: ErrorEvent | Error) => void
-    ): void;
-  }
 
   export interface FontData {
-    glyphs: Record<string, unknown>; // ✅ dari any → Record<string, unknown>
+    glyphs: Record<string, unknown>;
     familyName: string;
     ascender: number;
     descender: number;
@@ -25,7 +15,7 @@ declare module 'three/examples/jsm/loaders/FontLoader' {
       xMax: number;
     };
     resolution: number;
-    original_font_information: Record<string, unknown>; // ✅ dari any → Record<string, unknown>
+    original_font_information: Record<string, unknown>;
     cssFontWeight: string;
     cssFontStyle: string;
   }
@@ -33,5 +23,14 @@ declare module 'three/examples/jsm/loaders/FontLoader' {
   export class Font {
     constructor(data: FontData);
     data: FontData;
+  }
+
+  export class FontLoader extends Loader {
+    load(
+      url: string,
+      onLoad: (font: Font) => void,
+      onProgress?: (event: ProgressEvent<EventTarget>) => void,
+      onError?: (event: ErrorEvent | Error) => void
+    ): void;
   }
 }
